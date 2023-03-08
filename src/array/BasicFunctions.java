8f7a5676198d31f866d34d6cf6e39dbae86cbfdf
kaushik <kauhshik116k@gmail.com>
2023-03-08 15:44:03 +0530

    Adding comments for reversing an array

--- src/array/BasicFunctions.java
+++ src/array/BasicFunctions.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 public class BasicFunctions {
     static Scanner sc = new Scanner(System.in);
+
+    // This function is used to get an array
     public int[] dynamicArray(){
         System.out.println("Enter length of array");
         int length = sc.nextInt();
@@ -17,12 +19,15 @@ public class BasicFunctions {
 
         return arr;
     }
+
+    // This function is used to swap elements of an array
     public void swap(int[] arr, int first, int last) {
         int temp = arr[first];
         arr[first] = arr[last];
         arr[last] = temp;
     }
 
+    // Printing an array
     public void print(int arr[]){
         for(int element : arr){
             System.out.print(element + " ");
--- src/array/ReverseAnArray.java
+++ src/array/ReverseAnArray.java
@@ -1,11 +1,15 @@
 package array;
-import  java.util.*;
 public class ReverseAnArray {
     public static void main(String[] args) {
         BasicFunctions array = new BasicFunctions();
         int arr[] = array.dynamicArray();
         int length = arr.length;
 
+        /*
+            -- Two pointers Solution
+            -- swap first and last elements
+            -- till the pointers meet each other
+         */
         int first = 0, last = length - 1;
         while(first <= last){
             array.swap(arr, first, last);
